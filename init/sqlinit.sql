11e9020f8274e7d5ef293384bd6cc9d09d4b6169
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-01-14 13:23:10 +0100

    updated schema

--- init/sqlinit.sql
+++ init/sqlinit.sql
@@ -1,88 +1,69 @@
 CREATE DATABASE IF NOT EXISTS DiscordStats;
 USE DiscordStats;
-CREATE USER IF NOT EXISTS 'DiscordStatsBot'@'localhost' IDENTIFIED BY '1234';
-GRANT SELECT, INSERT, UPDATE, DELETE, DROP, TRIGGER ON DiscordStats.* TO 'DiscordStatsBot'@'localhost';
+CREATE USER IF NOT EXISTS 'DiscordStatsBot'@'localhost' IDENTIFIED WITH mysql_native_password BY '1234';
+GRANT SELECT, INSERT, UPDATE, DELETE, DROP ON DiscordStats.* TO 'DiscordStatsBot'@'localhost';
 GRANT CREATE VIEW, CREATE USER ON *.* TO 'DiscordStatsBot'@'localhost';
 FLUSH PRIVILEGES;
 
 CREATE TABLE IF NOT EXISTS Users(
-    id          INT             NOT NULL PRIMARY KEY,
+    id          BIGINT             NOT NULL PRIMARY KEY,
     userName    VARCHAR(32)     NOT NULL,
-    userTage    INT             NOT NULL
+    userTag     BIGINT             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Guilds(
-    id          INT             NOT NULL PRIMARY KEY,
+    id          BIGINT             NOT NULL PRIMARY KEY,
     name        VARCHAR(100)    NOT NULL,
-    ownerID     INT             NOT NULL
+    ownerID     BIGINT             NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS GuildUser(
-    guildId     INT             NOT NULL,
-    userId      INT             NOT NULL,
-    nickname    VARCHAR(100)    NOT NULL,
-
-    FOREIGN KEY (guildId)
-        REFERENCES Guilds(id)
-        ON DELETE CASCADE,
-
-    FOREIGN KEY (userId)
-        REFERENCES Users(id)
-        ON DELETE CASCADE
+    guildId     BIGINT             NOT NULL,
+    userId      BIGINT             NOT NULL,
+    nickname    VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS Channels(
-    id          INT                     NOT NULL PRIMARY KEY,
+    id          BIGINT                     NOT NULL PRIMARY KEY,
     name        VARCHAR(100)            NOT NULL,
-    guildId     INT                     NOT NULL,
-    type        ENUM('Voice', 'Text')   NOT NULL,
-
-    FOREIGN KEY (guildId)
-        REFERENCES Guilds(id)
-        ON DELETE CASCADE
+    guildId     BIGINT                     NOT NULL,
+    type        ENUM('Voice', 'Text')   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Messages(
-    id      INT     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    time    TIME    NOT NULL,
-    author  INT     NOT NULL,
-    channel INT     NOT NULL,
-
-    FOREIGN KEY (author)
-        REFERENCES GuildUser(userId)
-        ON DELETE CASCADE,
-
-    FOREIGN KEY (channel)
-        REFERENCES Channels(id)
-        ON DELETE CASCADE
+    id      BIGINT     NOT NULL,
+    DATETIME   DATETIME   NOT NULL,
+    author  BIGINT     NOT NULL,
+    channel BIGINT     NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ChannelLog(
     event       ENUM('ChannelNameChanged', 'ChannelCreated', 'ChannelDeleted')  NOT NULL,
-    guildId     INT                                                             NOT NULL,
+    guildId     BIGINT                                                             NOT NULL,
     name        VARCHAR(100)                                                    NOT NULL,
-    time        TIME                                                            NOT NULL
+    DATETIME       DATETIME                                                           NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS GuildLog(
     event       ENUM('GuildNameChanged','GuildCreated', 'GuildDeleted') NOT NULL,
-    guildId     INT                                                     NOT NULL,
+    guildId     BIGINT                                                     NOT NULL,
     name        VARCHAR(100)                                            NOT NULL,
-    time        TIME                                                    NOT NULL
+    DATETIME       DATETIME                                                   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS GuildUserLog(
     event       ENUM('GuildUserNameChanged', 'GuildUserCreated', 'GuildUserDeleted')    NOT NULL,
-    guildUser   INT                                                                     NOT NULL,
+    guildUser   BIGINT                                                                     NOT NULL,
     nickname    VARCHAR(100)                                                            NOT NULL,
-    time        TIME                                                                    NOT NULL
+    DATETIME       DATETIME                                                                   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS MessageLog(
     event       ENUM('MessageEdited', 'MessageDeleted') NOT NULL,
-    channelId   INT                                     NOT NULL,
-    guildUser   INT                                     NOT NULL,
-    time        TIME                                    NOT NULL
+    id          BIGINT                                    NOT NULL,
+    channel   BIGINT                                     NOT NULL,
+    user   BIGINT                                     NOT NULL,
+    DATETIME       DATETIME                                      NOT NULL
 );
 
 DELIMITER //
