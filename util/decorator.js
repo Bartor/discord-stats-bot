b9fdf52431773eb81f9109c8cf65feb287e0d972
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-01-15 17:26:23 +0100

    decorator accepts null now

--- util/decorator.js
+++ util/decorator.js
@@ -1,5 +1,5 @@
 module.exports = function(fields, rows) {
-    let MAX_LENGTH = 12;
+    let MAX_LENGTH = 20;
 
     let string = '\n```html\n';
     for (let f of fields) string += `${('<'+f.name+'>').substring(0, MAX_LENGTH).padEnd(MAX_LENGTH, ' ')}`;
@@ -7,7 +7,7 @@ module.exports = function(fields, rows) {
     for (let r of rows) {
         for (let p in r) {
             if (r.hasOwnProperty(p)) {
-                string += `${r[p].toString().substring(0, MAX_LENGTH).padEnd(MAX_LENGTH, ' ')}` ;
+                string += `${r[p] !== null ? r[p].toString().substring(0, MAX_LENGTH).padEnd(MAX_LENGTH, ' ') : 'NULL'.padEnd(MAX_LENGTH, ' ')}` ;
             }
         }
         string += '\n';
