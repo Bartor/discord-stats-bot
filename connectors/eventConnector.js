c35fec730f1b0eac590dbd2f8816da0d26108648
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-01-15 17:53:42 +0100

    changed to apply to new sql schema

--- connectors/eventConnector.js
+++ connectors/eventConnector.js
@@ -63,9 +63,9 @@ module.exports = {
         connection.query('INSERT INTO GuildUser VALUES(?, ?, ?)', [gid, uid, name], cb);
     },
     updateGuildUser: function(gid, uid, name, cb) {
-        connection.query('UPDATE GuildUser SET nickname = ? WHERE guildId = ? AND userId = ?', [name, gid, uid], cb);
+        connection.query('UPDATE GuildUser SET nickname = ? WHERE guildId = ? AND user = ?', [name, gid, uid], cb);
     },
     deleteGuildUser: function(gid, uid, cb) {
-        connection.query('DELETE FROM GuildUser WHERE guildId = ? AND userId = ?', [gid, uid], cb);
+        connection.query('DELETE FROM GuildUser WHERE guildId = ? AND user = ?', [gid, uid], cb);
     }
 }
