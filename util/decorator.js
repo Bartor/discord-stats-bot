78a1d9abbc2a2d2dca3818bbd0e194a9af0e2bdc
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-01-15 17:52:10 +0100

    decorator handles date and trims ends of lines

--- util/decorator.js
+++ util/decorator.js
@@ -2,15 +2,21 @@ module.exports = function(fields, rows) {
     let MAX_LENGTH = 20;
 
     let string = '\n```html\n';
-    for (let f of fields) string += `${('<'+f.name+'>').substring(0, MAX_LENGTH).padEnd(MAX_LENGTH, ' ')}`;
-    string += '\n\n';
+    let localString = '';
+    for (let f of fields) localString += `${('<'+f.name+'>').substring(0, MAX_LENGTH).padEnd(MAX_LENGTH, ' ')}`;
+    string += `${localString.trim()}\n\n`;
     for (let r of rows) {
+        localString = '';
         for (let p in r) {
             if (r.hasOwnProperty(p)) {
-                string += `${r[p] !== null ? r[p].toString().substring(0, MAX_LENGTH).padEnd(MAX_LENGTH, ' ') : 'NULL'.padEnd(MAX_LENGTH, ' ')}` ;
+                localString += `${r[p] !== null ?
+                     r[p] instanceof Date ? 
+                     r[p].getDate() + '-' + (r[p].getMonth()+1) + '-' + (r[p].getYear()-100) + ' ' + r[p].getHours() + ':' + r[p].getMinutes() + ':' + r[p].getSeconds()
+                      : r[p].toString().substring(0, MAX_LENGTH).padEnd(MAX_LENGTH, ' ') 
+                    : 'NULL'.padEnd(MAX_LENGTH, ' ')}`;
             }
         }
-        string += '\n';
+        string += `${localString.trim()}\n`;
     }
     string += '```';
     return string;
