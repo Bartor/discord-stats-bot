49aa471cde4e22e5651a2724cbcbc48bd329b9e5
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-01-17 18:24:36 +0100

    added daily guild preview

--- connectors/webConnector.js
+++ connectors/webConnector.js
@@ -22,22 +22,13 @@ module.exports = {
     getAllChannels: function(guildId, cb) {
         connection.query('SELECT C.name AS channelName, C.id AS channelId, G.name AS guildName, C.type AS type FROM Channels C JOIN Guilds G ON C.guildId = G.id WHERE guildId = ?', [guildId], cb);
     },
-    getAllGuildUsers: function(guildId, cb) {
-        connection.query('SELECT * FROM GuildUser GU JOIN U ON GU.user = U.id WHERE guildId = ?', [guildId], cb);
-    },
     getAllGuilds: function(cb) {
         connection.query('SELECT * FROM Guilds', cb);
     },
-    getAllDelsCount: function(channelId, cb) {
-        connection.query('SELECT COUNT(*) AS Count FROM MessageLog WHERE channel = ? AND Event = "MessageDeleted"', [channelId], cb);
-    },
-    getAllEditsCount: function(channelId, cb) {
-        connection.query('SELECT COUNT(*) AS Count FROM MessageLog WHERE channel = ? AND Event = "MessageEdited"', [channelId], cb);
-    },
-    getAllMessCount: function(channelId, cb) {
-        connection.query('SELECT COUNT(*) AS Count FROM Messages WHERE channel = ?', [channelId], cb);
-    },
     getMessFromWeek: function(guildId, cb) {
         connection.query('SELECT M.time AS time FROM Messages M JOIN Channels C ON M.channel = C.id JOIN Guilds G ON C.guildID = G.id WHERE time > DATE_SUB(NOW(), INTERVAL 1 week) AND guildId = ?', [guildId], cb);
-    }
+    },
+    getMessFromDay: function(guildId, day, cb) {
+        connection.query('SELECT time, userName, G.name AS guildName FROM Messages M JOIN Channels C ON M.channel = C.id JOIN Guilds G ON C.guildId = G.id JOIN Users U ON M.author = U.id WHERE G.id = ? AND DAY(M.time) = DAY(?)', [guildId, day], cb);
+    }   
 }
--- web/routers/index.js
+++ web/routers/index.js
@@ -17,12 +17,12 @@ router.get('/g/:id/', (req, res) => {
     connector.getAllChannels(req.params.id, (err, channels, fields) => {
         if (err) {
             console.log(err); 
-            res.render('guild', {data: []});
+            res.sendStatus(500);
         } else {
             connector.getMessFromWeek(req.params.id, (err, days, fields) => {
                 if (err) {
                     console.log(err);
-                    res.render('guild', {data: []});
+                    res.sendStatus(500);
                 } else {
                     let weekStats = [];
                     for (let i = 0; i < 7; i++) {
@@ -45,32 +45,33 @@ router.get('/g/:id/', (req, res) => {
     });
 });
 
-router.get('/c/:id/', (req, res) => {
-    connector.getAllDelsCount(req.params.id, (err, rows, fields) => {
+router.get('/g/:id/day/:time/', (req, res) => {
+    let d = new Date(new Number(req.params.time).valueOf());
+    connector.getMessFromDay(req.params.id, d, (err, rows, fields) => {
         if (err) {
-            console.log(err); 
-            res.render('channel', {status: false, text: "", data: []});
+            console.log(err);
+            res.sendStatus(500);
         } else {
-            let data = rows.map(e => ({name: "Deletions", count: e.Count}));
-            connector.getAllEditsCount(req.params.id, (err, rows, fields) => {
-                if (err) {
-                    console.log(err); 
-                    res.render('channel', {status: false, text: "", data: []});
-                } else {
-                    data = data.concat(rows.map(e => ({name: "Edits", count: e.Count})));
-                    connector.getAllMessCount(req.params.id, (err, rows, fields) => {
-                        if (err) {
-                            console.log(err); 
-                            res.render('channel', {status: false, text: "", data: []});
-                        } else {
-                            data = data.concat(rows.map(e => ({name: "Messages", count: e.Count})));
-                            res.render('channel', {status: true, text: "Channel information:", data: data});
-                        }        
-                    });
-                }        
-            });
+            let hourStats = [];
+            for (let i = 0; i < 24; i++) {
+                hourStats.push([
+                    i + ":00 - " + (i+1) + ":00",
+                    rows.filter(e => new Date(e.time).getHours() === i).length
+                ]);
+            }
+            let topUsers = {};
+            for (let m of rows) {
+                if (topUsers[m.userName] !== undefined) topUsers[m.userName]++;
+                else topUsers[m.userName] = 0;
+            }
+            topUsers = Object.entries(topUsers).sort((a, b) => b[1] - a[1]);
+            res.render('guildDay', {day: `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`,guildId: req.params.id, guild: rows[0].guildName, stats: hourStats, users: topUsers, maxStat: Math.max(...topUsers.map(e => e[1])), maxUser: Math.max(...hourStats.map(e => e[1]))});
         }
-    })
-})
+    });
+});
+
+router.get('/c/:id/', (req, res) => {
+
+});
 
 module.exports = router;
